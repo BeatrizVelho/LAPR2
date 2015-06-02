a7db4a52380cb370d3e15cf935297213a960be59
Soraia Freitas <1140805@isep.ipp.pt>
2015-06-02 14:58:13 +0100

    Criação da classe RegistoUtilizador: - métodos método NovoUtilizador e ValidaUtilizador classe teste Utilizador: acrescenteio metodo validaUtilizador

---
+++ src/eventoscientificos/model/RegistoUtilizador.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventoscientificos.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author G01
+ */
+public class RegistoUtilizador {
+
+    private List<Utilizador> listaUtilizadores;
+
+    public RegistoUtilizador() {
+
+        this.listaUtilizadores = new ArrayList<>();
+    }
+
+    public Utilizador novoUtilizador(String nome, String email, String username, String password) {
+        boolean utilizadorValido = false;
+        Utilizador u = new Utilizador(nome, email, username, password);
+        try {
+            if (u.validaUtilizador() && validarUtilizador(u)) {
+                utilizadorValido = true;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Utilizador invalido!");
+        }
+        if (utilizadorValido == true) {
+            return u;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean adicionaUtilizador(Utilizador u) {
+        if (this.listaUtilizadores.add(u)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean validarUtilizador(Utilizador u) {
+        if (!listaUtilizadores.contains(u)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        RegistoUtilizador ru = new RegistoUtilizador();
+        Utilizador u = ru.novoUtilizador("susana", "mail@mail.com", "susu", "1234");
+        boolean adicionado = ru.adicionaUtilizador(u);
+        System.out.println(adicionado);
+    }
+
+}
