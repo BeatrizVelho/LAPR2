30b296a15e3abb8872cce97ae07b4aeae4ad7d89
Pedro Moreira <1140781@isep.ipp.pt>
2015-06-02 11:10:58 +0100

    Implementação de mais 3 testes feitos a classe Proponente, os testes foram feitos ao método getUtilizador() e ao método equals().

--- test/eventoscientificos/model/ProponenteTest.java
+++ test/eventoscientificos/model/ProponenteTest.java
@@ -7,8 +7,20 @@ import static org.junit.Assert.*;
  * @author G01
  */
 public class ProponenteTest {
-    
+
     public ProponenteTest() {
+    }
+
+    /**
+     * Teste do método getUtilizador, da classe Proponente.
+     */
+    @Test
+    public void testGetUtilizador() {
+        System.out.println("getUtilizador");
+        Proponente instance = new Proponente(new Utilizador());
+        Utilizador expResult = new Utilizador();
+        Utilizador result = instance.getUtilizador();
+        assertEquals(expResult, result);
     }
 
     /**
@@ -22,9 +34,9 @@ public class ProponenteTest {
         boolean result = instance.validarProponente();
         assertEquals(expResult, result);
     }
-    
+
     /**
-     * Teste do método validarProponente, quando não é válido, da classe 
+     * Teste do método validarProponente, quando não é válido, da classe
      * Proponente.
      */
     @Test(expected = NullPointerException.class)
@@ -33,5 +45,33 @@ public class ProponenteTest {
         Proponente instance = new Proponente(null);
         instance.validarProponente();
     }
-    
+
+    /**
+     * Teste do método equals, da classe Proponente.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        Object outroObjeto = new Proponente(new Utilizador());
+        Proponente instance = new Proponente(new Utilizador());
+        boolean expResult = true;
+        boolean result = instance.equals(outroObjeto);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método equals, da classe Proponente, para proponentes
+     * diferentes.
+     */
+    @Test
+    public void testEqualsNot() {
+        System.out.println("equalsNot");
+        Object outroObjeto = new Proponente(new Utilizador());
+        Proponente instance = new Proponente(new Utilizador(
+                "Pedro", "1140781@isep.ipp.pt", "pedro", "1234"));
+        boolean expResult = false;
+        boolean result = instance.equals(outroObjeto);
+        assertEquals(expResult, result);
+    }
+
 }
