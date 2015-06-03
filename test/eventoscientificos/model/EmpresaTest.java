e2b4b483530063ac1690a9fda92dbf3191bfac9d
Beatriz Velho <1140587@isep.ipp.pt>
2015-06-03 19:55:31 +0100

    Adicionado o teste do metodo getRegistoEventos

--- test/eventoscientificos/model/EmpresaTest.java
+++ test/eventoscientificos/model/EmpresaTest.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eventoscientificos.model;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
  * @author G01
  */
 public class EmpresaTest {
 
     /**
-     * Construtor da empresa teste
+     * Construtor da empresa teste.
      */
     public EmpresaTest() {
     }
@@ -24,12 +18,24 @@ public class EmpresaTest {
      * Testa o metodo getRegistoUtilizadores da classe Empresa.
      */
     @Test
-    public void testGetRegistoUtilizador() {
-        System.out.println("getRegistoUtilizador");
+    public void testGetRegistoUtilizadores() {
+        System.out.println("getRegistoUtilizadores");
         Empresa instance = new Empresa();
         RegistoUtilizadores expResult = new RegistoUtilizadores();
         RegistoUtilizadores result = instance.getRegistoUtilizadores();
         assertEquals(expResult, result);
     }
-
+    
+    /**
+     * Testa o metodo getRegistoEventos da classe Empresa.
+     */
+    @Test
+    public void testGetRegistoEventos() {
+        System.out.println("getRegistoEventos");
+        Empresa instance = new Empresa();
+        RegistoEventos expResult = new RegistoEventos();
+        RegistoEventos result = instance.getRegistoEventos();
+        assertEquals(expResult, result);
+    }
+    
 }
