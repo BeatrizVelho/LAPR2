093f9509f77e48f0d56df14aba947b7145e93d83
TimiFerreira <1131658@isep.ipp.pt>
2015-06-03 17:35:46 +0100

    Implementação na classe de testes da classe CP (equals, validarCP).

--- test/eventoscientificos/model/CPTest.java
+++ test/eventoscientificos/model/CPTest.java
@@ -38,4 +38,56 @@ public class CPTest {
         instance.novoRevisor(new Utilizador(
                 "Tiago", "1131658@isep.ipp.pt", "tiago", "1234"));
     }
+
+    /**
+     * Teste do método equals, da classe CP.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        Object outroObjeto = new CP();
+        CP instance = new CP();
+        boolean expResult = true;
+        assertEquals(expResult, instance.equals(outroObjeto));
+    }
+
+    /**
+     * Teste do método equals, da classe CP.
+     */
+    @Test
+    public void testEqualsNot() {
+        System.out.println("equalsNot");
+        CP outraCP = new CP();
+        outraCP.novoRevisor(new Utilizador(
+                "tiago", "1131658@isep.ipp.pt", "tiago", "1234"));
+        CP instance = new CP();
+        boolean expResult = false;
+        assertEquals(expResult, instance.equals(outraCP));
+    }
+
+    /**
+     * Teste do método validarCP, da classe CP.
+     */
+    @Test
+    public void testValidarCP() {
+        System.out.println("validarCPVazia");
+        CP instance = new CP();
+        instance.novoRevisor(new Utilizador(
+                "tiago", "1131658@isep.ipp.pt", "tiago", "1234"));
+        boolean expResult = true;
+        boolean result = instance.validarCP();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método validarCPVazia, da classe CP.
+     */
+    @Test
+    public void testValidarCPVazia() {
+        System.out.println("validarCPVazia");
+        CP instance = new CP();
+        boolean expResult = false;
+        boolean result = instance.validarCP();
+        assertEquals(expResult, result);
+    }
 }
