5e81c02324029ba3905df6037e6c15ed8424c503
Pedro Moreira <1140781@isep.ipp.pt>
2015-06-04 09:15:19 +0100

    Implementada a classe ListaSessoesTematicas com os métodos novaSessaoTematica, validarSessaoTematica e adicionarSessaoTematica.

---
+++ src/eventoscientificos/model/ListaSessoesTematicas.java
@@ -0,0 +1,72 @@
+package eventoscientificos.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import utils.Data;
+
+/**
+ * Representa uma instância de uma lista de Sessões Temáticas através dessa
+ * mesma lista.
+ *
+ * @author G01
+ */
+public class ListaSessoesTematicas {
+
+    /**
+     * Lista de Sessões Temáticas.
+     */
+    private List<SessaoTematica> listaSessoesTematicas;
+
+    /**
+     * Constrói uma instância de uma lista de Sessões Temáticas vazia.
+     */
+    public ListaSessoesTematicas() {
+        this.listaSessoesTematicas = new ArrayList();
+    }
+
+    /**
+     * Cria uma instância de sessão temática com um código único, uma descrição,
+     * uma data de inicio de submissão e data de fim de submissão.
+     * 
+     * @param codigoUnico Código único da Sessão Temática.
+     * @param descricao Descrição da Sessão Temátiica.
+     * @param dataInicioSubmissao Data de inicio do periodo de submissão da 
+     * Sessão Temática.
+     * @param dataFimSubmissao Data de fim do periodo de submissão da Sessão
+     * Temática.
+     * @return Sessão Temática.
+     */
+    public SessaoTematica novaSessaoTematica(
+            String codigoUnico,
+            String descricao,
+            Data dataInicioSubmissao,
+            Data dataFimSubmissao) {
+        return new SessaoTematica(
+                codigoUnico, descricao, dataInicioSubmissao, dataFimSubmissao);
+    }
+
+    /**
+     * Valida uma instância de Sessão Temática verificando se a mesma já existe
+     * numa lista.
+     * 
+     * @param sessaoTematica Sessão Temática que vai ser procurada na lista.
+     * @return Verdadeiro se a Sessão Temática já existir na lista e falso caso
+     * não exista.
+     */
+    public boolean validarSessaoTematica(SessaoTematica sessaoTematica) {
+        return !this.listaSessoesTematicas.contains(sessaoTematica);
+    }
+
+    /**
+     * 
+     * Adiciona uma instância de uma Sessão Temática a uma lista.
+     * 
+     * @param sessaoTematica Sessão Temática que vai ser adicionada à lista.
+     * @return Verdadeiro caso a Sessão Temática seja adicionada à lista e falso
+     * caso a adição falhe.
+     */
+    public boolean adicionarSessaoTematica(SessaoTematica sessaoTematica) {
+        return this.listaSessoesTematicas.add(sessaoTematica);
+    }
+
+}
