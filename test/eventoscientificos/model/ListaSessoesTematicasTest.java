bbab43e332b360c07b5355a1661e9064cdd755a9
Pedro Moreira <1140781@isep.ipp.pt>
2015-06-04 11:31:08 +0100

    Melhorados os testes da classe ListaSessoesTematicas e adicionados dois testes ao método equals.

--- test/eventoscientificos/model/ListaSessoesTematicasTest.java
+++ test/eventoscientificos/model/ListaSessoesTematicasTest.java
@@ -9,8 +9,15 @@ import utils.Data;
  * @author G01
  */
 public class ListaSessoesTematicasTest {
+    
+    private SessaoTematica sessaoTematica;
 
     public ListaSessoesTematicasTest() {
+        this.sessaoTematica = new SessaoTematica(
+                "#123456",
+                "Uma descrição",
+                new Data(2016, 05, 22),
+                new Data(2017, 05, 22));
     }
 
     /**
@@ -20,20 +27,12 @@ public class ListaSessoesTematicasTest {
     public void testNovaSessaoTematica() {
         ListaSessoesTematicas instance = new ListaSessoesTematicas();
         System.out.println("novaSessaoTematica");
-        String codigoUnico = "#123456";
-        String descricao = "Uma descrição";
-        Data dataInicioSubmissao = new Data(2016, 05, 22);
-        Data dataFimSubmissao = new Data(2017, 05, 22);
-        SessaoTematica expResult = new SessaoTematica(
-                codigoUnico,
-                descricao,
-                dataInicioSubmissao,
-                dataFimSubmissao);
+        SessaoTematica expResult = this.sessaoTematica;
         SessaoTematica result = instance.novaSessaoTematica(
-                codigoUnico,
-                descricao,
-                dataInicioSubmissao,
-                dataFimSubmissao);
+                "#123456",
+                "Uma descrição",
+                new Data(2016, 05, 22),
+                new Data(2017, 05, 22));
         assertEquals(expResult, result);
     }
 
@@ -43,11 +42,7 @@ public class ListaSessoesTematicasTest {
     @Test
     public void testAdicionarSessaoTematica() {
         System.out.println("adicionarSessaoTematica");
-        SessaoTematica sessaoTematica = new SessaoTematica(
-                "#123456",
-                "Uma descrição",
-                new Data(2016, 05, 22),
-                new Data(2017, 05, 22));
+        SessaoTematica sessaoTematica = this.sessaoTematica;
         ListaSessoesTematicas instance = new ListaSessoesTematicas();
         boolean expResult = true;
         boolean result = instance.adicionarSessaoTematica(sessaoTematica);
@@ -61,11 +56,7 @@ public class ListaSessoesTematicasTest {
     public void testValidarSessaoTematica() {
         System.out.println("validarSessaoTematica");
         ListaSessoesTematicas instance = new ListaSessoesTematicas();
-        SessaoTematica sessaoTematica = new SessaoTematica(
-                "#123456",
-                "Uma descrição",
-                new Data(2016, 05, 22),
-                new Data(2017, 05, 22));
+        SessaoTematica sessaoTematica = this.sessaoTematica;
         boolean expResult = true;
         boolean result = instance.validarSessaoTematica(sessaoTematica);
         assertEquals(expResult, result);
@@ -78,14 +69,36 @@ public class ListaSessoesTematicasTest {
     public void testValidarSessaoTematicaNot() {
         System.out.println("validarSessaoTematicaNot");
         ListaSessoesTematicas instance = new ListaSessoesTematicas();
-        SessaoTematica sessaoTematica = new SessaoTematica(
-                "#123456",
-                "Uma descrição",
-                new Data(2016, 05, 22),
-                new Data(2017, 05, 22));
+        SessaoTematica sessaoTematica = this.sessaoTematica;
         instance.adicionarSessaoTematica(sessaoTematica);
         boolean expResult = false;
         boolean result = instance.validarSessaoTematica(sessaoTematica);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método equals, da classe ListaSessoesTematicas.
+     */
+    @Test
+    public void testEquals() {
+        Object outroObject = new ListaSessoesTematicas();
+        ListaSessoesTematicas instance = new ListaSessoesTematicas();
+        boolean expResult = true;
+        boolean result = instance.equals(outroObject);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Teste do método equals, da classe ListaSessoesTematicas.
+     */
+    @Test
+    public void testEqualsNot() {
+        System.out.println("equalsNot");
+        Object outroObject = new ListaSessoesTematicas();
+        ListaSessoesTematicas instance = new ListaSessoesTematicas();
+        instance.adicionarSessaoTematica(this.sessaoTematica);
+        boolean expResult = false;
+        boolean result = instance.equals(outroObject);
         assertEquals(expResult, result);
     }
 
