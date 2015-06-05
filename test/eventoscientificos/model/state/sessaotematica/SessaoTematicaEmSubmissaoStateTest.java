6bb1d09dcdbdb5144dace83d2a525ff256db8152
Pedro Moreira <1140781@isep.ipp.pt>
2015-06-05 13:38:05 +0100

    Criados os testes da classe SessaoTematicaCriadaState, os testes foram feitos aos métodos: setCriada, setRegistada, setCPDefinida, setEmSubmissao, setEmDetecao, setEmLicitacao, setEmDistribuicao, setEmRevisao, setFaseDecisao, setEmSubmissaoCameraReady, setCameraReady e validarEstado.

---
+++ test/eventoscientificos/model/state/sessaotematica/SessaoTematicaEmSubmissaoStateTest.java
@@ -0,0 +1,182 @@
+package eventoscientificos.model.state.sessaotematica;
+
+import eventoscientificos.model.SessaoTematica;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import utils.Data;
+
+/**
+ *
+ * @author G01
+ */
+public class SessaoTematicaEmSubmissaoStateTest {
+    
+    private SessaoTematica sessaoTematica;
+    
+    public SessaoTematicaEmSubmissaoStateTest() {
+        SessaoTematica sessaoTematica = new SessaoTematica(
+                "#12345",
+                "Um descrição",
+                new Data(2015, 05, 22),
+                new Data(2015, 05, 28));
+        sessaoTematica.setEstado(
+                new SessaoTematicaEmSubmissaoState(sessaoTematica));
+
+        this.sessaoTematica = sessaoTematica;
+    }
+
+    /**
+     * Teste do método setCriada, da classe SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetCriada() {
+        System.out.println("setCriada");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setCriada();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método setRegistada, da classe
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetRegistada() {
+        System.out.println("setRegistada");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setRegistada();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test do método setCPDefinida, da classe
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetCPDefinida() {
+        System.out.println("setCPDefinida");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setCPDefinida();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método setEmSubmissao, da classe 
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetEmSubmissao() {
+        System.out.println("setEmSubmissao");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = true;
+        boolean result = instance.setEmSubmissao();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setEmDetecao method, of class SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetEmDetecao() {
+        System.out.println("setEmDetecao");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = true;
+        boolean result = instance.setEmDetecao();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do méotodo setEmLicitacao, da classe 
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetEmLicitacao() {
+        System.out.println("setEmLicitacao");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setEmLicitacao();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método setEmDistribuicao, da classe 
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetEmDistribuicao() {
+        System.out.println("setEmDistribuicao");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setEmDistribuicao();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método setEmRevisao, da classe
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetEmRevisao() {
+        System.out.println("setEmRevisao");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setEmRevisao();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método setFaseDecisao, da classe
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetFaseDecisao() {
+        System.out.println("setFaseDecisao");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setFaseDecisao();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método setEmSubmissaoCameraReady, da classe
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetEmSubmissaoCameraReady() {
+        System.out.println("setEmSubmissaoCameraReady");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setEmSubmissaoCameraReady();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método setCameraReady, da classe
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testSetCameraReady() {
+        System.out.println("setCameraReady");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = false;
+        boolean result = instance.setCameraReady();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método validarEstado, da classe
+     * SessaoTematicaEmSubmissaoStateTest.
+     */
+    @Test
+    public void testValidarEstado() {
+        System.out.println("validarEstado");
+        SessaoTematicaState instance = this.sessaoTematica.getEstado();
+        boolean expResult = true;
+        boolean result = instance.validarEstado();
+        assertEquals(expResult, result);
+    }
+    
+}
