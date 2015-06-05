8f06f9756403927fb19a91321e09099441c7753b
Beatriz Velho <1140587@isep.ipp.pt>
2015-06-05 16:26:41 +0100

    Implementação do método validar instituicao de afiliacao na classe Teste da classe Instituicao de Afiliacao

--- test/eventoscientificos/model/InstituicaoAfiliacaoTest.java
+++ test/eventoscientificos/model/InstituicaoAfiliacaoTest.java
@@ -8,7 +8,10 @@ import static org.junit.Assert.*;
  */
 public class InstituicaoAfiliacaoTest {
 
+    private InstituicaoAfiliacao instituicaoAfiliacao;
+    
     public InstituicaoAfiliacaoTest() {
+        this.instituicaoAfiliacao = new InstituicaoAfiliacao("ISEP");
     }
 
     /**
@@ -17,23 +20,26 @@ public class InstituicaoAfiliacaoTest {
      */
     @Test
     public void testSetAndGetNomeInstituicaoAfiliacao() {
-        System.out.println("getNomeInstituicaoAfiliacao");
-        InstituicaoAfiliacao instance = new InstituicaoAfiliacao("ISEP");
+        System.out.println("setAndGetNomeInstituicaoAfiliacao");
+        InstituicaoAfiliacao instance = this.instituicaoAfiliacao;
         String expResult = "porto ISEP";
         instance.setNomeInstituicaoAfiliacao(expResult);
         String result = instance.getNomeInstituicaoAfiliacao();
         assertEquals(expResult, result);
     }
-
+    
     /**
-     * Teste do metodo setNomeInstituicaoAfiliacao, da classe InstituicaoAfiliacao.
+     * Teste do método setNomeInstituicaoAfiliacao, da classe
+     * InstituicaoAfiliacao.
      */
     @Test(expected = NullPointerException.class)
     public void testSetNomeInstituicaoAfiliacaoNull() {
-        System.out.println("setNomeInstituicaoAfiliacaoNull");
-        String local = null;
-        InstituicaoAfiliacao instance = new InstituicaoAfiliacao("isep");
-        instance.setNomeInstituicaoAfiliacao(local);
+        System.out.println("setNomeInstituicaoAfiliacao");
+        InstituicaoAfiliacao instance = null;
+        String expResult = "ISEP";
+        instance.setNomeInstituicaoAfiliacao(expResult);
+        String result = instance.getNomeInstituicaoAfiliacao();
+        assertEquals(expResult, result);
     }
 
     /**
@@ -42,9 +48,22 @@ public class InstituicaoAfiliacaoTest {
     @Test
     public void testToString() {
         System.out.println("toString");
-        InstituicaoAfiliacao instance = new InstituicaoAfiliacao("regua");
-        String expResult = "regua";
+        InstituicaoAfiliacao instance = this.instituicaoAfiliacao;
+        String expResult = "ISEP";
         String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método validarInstituicaoAfiliacao, 
+     * da classe InstituicaoAfiliacao.
+     */
+    @Test
+    public void testValidarInstituicaoAfiliacao() {
+        System.out.println("validarInstituicaoAfiliacao");
+        InstituicaoAfiliacao instance = this.instituicaoAfiliacao;
+        boolean expResult = true;
+        boolean result = instance.validarInstituicaoAfiliacao();
         assertEquals(expResult, result);
     }
 
