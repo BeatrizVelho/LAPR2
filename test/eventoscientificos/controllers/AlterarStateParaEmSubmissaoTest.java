f49a9fd47355f862dd4d510e08d7d5fac692c0fa
Pedro Moreira <1140781@isep.ipp.pt>
2015-06-05 13:16:28 +0100

    Criados os testes da classe AlterarStateParaEmSubmissao, foi apenas feito um teste ao método run.
    Correção do commit anterior: 
    
    Implementada a classe AlterarStateParaEmSubmissao, com o método run.

---
+++ test/eventoscientificos/controllers/AlterarStateParaEmSubmissaoTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventoscientificos.controllers;
+
+import eventoscientificos.model.CP;
+import eventoscientificos.model.SessaoTematica;
+import eventoscientificos.model.StateSessaoTematica.SessaoTematicaCPDefinidaState;
+import eventoscientificos.model.StateSessaoTematica.SessaoTematicaEmSubmissaoState;
+import eventoscientificos.model.StateSessaoTematica.SessaoTematicaRegistadaState;
+import eventoscientificos.model.StateSessaoTematica.SessaoTematicaState;
+import eventoscientificos.model.Submissivel;
+import java.util.TimerTask;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import utils.Data;
+
+/**
+ *
+ * @author Pedro
+ */
+public class AlterarStateParaEmSubmissaoTest {
+
+    private SessaoTematica sessaoTematica;
+
+    public AlterarStateParaEmSubmissaoTest() {
+        SessaoTematica sessaoTematica = new SessaoTematica(
+                "#A9D24R",
+                "LAPR2",
+                new Data(2015, 5, 22),
+                new Data(2015, 5, 28));
+        sessaoTematica.setEstado(
+                new SessaoTematicaCPDefinidaState(sessaoTematica));
+
+        this.sessaoTematica = sessaoTematica;
+    }
+
+    /**
+     * Teste do método run, da classe AlterarStateParaEmSubmissao.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        TimerTask instance = new AlterarStateParaEmSubmissao(
+                (Submissivel) this.sessaoTematica);
+        instance.run();
+        Class<? extends SessaoTematicaState> expResult
+                = new SessaoTematicaEmSubmissaoState(sessaoTematica).getClass();
+        Class<? extends SessaoTematicaState> result
+                = this.sessaoTematica.getEstado().getClass();
+        assertEquals(expResult, result);
+    }
+
+}
