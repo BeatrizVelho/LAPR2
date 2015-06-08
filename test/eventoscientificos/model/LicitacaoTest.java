cd4598545f5a1701830b3297fd379f8dac067c7e
Soraia Freitas <1140805@isep.ipp.pt>
2015-06-08 20:23:07 +0100

    implementação da classe teste da classe licitação com os seus devidos teste efetuados e passados

---
+++ test/eventoscientificos/model/LicitacaoTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventoscientificos.model;
+
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Testa a classe Licitação
+ *
+ * @author G01
+ */
+public class LicitacaoTest {
+
+    /**
+     * Revisor que licita.
+     */
+    private Revisor revisor;
+    /**
+     * Artigo a licitar.
+     */
+    private Artigo artigo;
+    /**
+     * Grau de interesse em rever.
+     */
+    private int grauInteresse;
+    /**
+     * Lista de conflitos de interesse entre o revisor e o artigo a rever
+     */
+    private List<Conflito> listaConflitos;
+
+    /**
+     * Constrói uma instancia de LicitacaoTest.
+     */
+    public LicitacaoTest() {
+        Licitacao instance = new Licitacao(new Revisor(new Utilizador(
+                            "fatima", "ola@iml.com", "fafa", "1234")),
+                            new Artigo(), 0, null);
+    }
+
+    /**
+     * Test of setGrauInteresse method, of class Licitacao.
+     */
+    @Test
+    public void testSetGrauInteresse() {
+        System.out.println("setGrauInteresse");
+        int grauInteresse = 3;
+        Licitacao instance = new Licitacao(new Revisor(new Utilizador(
+                            "fatima", "ola@iml.com", "fafa", "1234")),
+                            new Artigo(), 0, null);
+
+        instance.setGrauInteresse(grauInteresse);
+    }
+
+    /**
+     * Test of setListaConflitos method, of class Licitacao.
+     */
+    @Test
+    public void testSetListaConflitos() {
+        System.out.println("setListaConflitos");
+        List<Conflito> listaConflitos = null;
+        Licitacao instance = new Licitacao(new Revisor(new Utilizador(
+                            "fatima", "ola@iml.com", "fafa", "1234")),
+                            new Artigo(), 0, null);
+        instance.setListaConflitos(listaConflitos);
+
+    }
+
+    /**
+     * Test of validarLicitacao method, of class Licitacao.
+     */
+    @Test
+    public void testValidarLicitacao() {
+        System.out.println("validarLicitacao");
+        Licitacao instance = new Licitacao(new Revisor(new Utilizador(
+                            "fatima", "ola@iml.com", "fafa", "1234")),
+                            new Artigo(), 0, null);
+        boolean expResult = true;
+        boolean result = instance.validarLicitacao();
+        assertEquals(expResult, result);
+
+    }
+
+}
