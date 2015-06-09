5097b0efeba29deb7fd33ee3249aa4d5f3e3ebd5
Soraia Freitas <1140805@isep.ipp.pt>
2015-06-09 08:56:24 +0100

    classe provisória apenas concebida poder compilar a classe Licitação. A classe conflito posteriormente será implementada.

---
+++ src/eventoscientificos/model/Conflito.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventoscientificos.model;
+
+/**
+ * Representa uma instância de Conflito através do nome.
+ *
+ * @author G01
+ */
+public class Conflito {
+
+}
