c17480d221f21b4a8f6fc3698d842275f2c02c93
Pedro Moreira <1140781@isep.ipp.pt>
2015-06-15 14:31:34 +0100

    Implementado o método setEmDetecao na Interface Detetavel.

--- src/eventoscientificos/model/Detetavel.java
+++ src/eventoscientificos/model/Detetavel.java
@@ -6,31 +6,40 @@ import java.util.List;
  * Todas as classes que implementam esta interface, herdam a responsabilidade de
  * guardar todos os conflitos detetados entre revisores e artigos, ficando
  * obrigadas a implementar um processo de deteção.
- * 
+ *
  * @author G01
  */
 public interface Detetavel {
 
     /**
      * Devolve o Processo de Deteção.
-     * 
+     *
      * @return Processo de Deteção.
      */
     ProcessoDetecao getProcessoDetecao();
 
     /**
      * Modifica o Processo de Deteção.
-     * 
+     *
      * @param processoDetecao Novo Processo de Deteção.
      */
     void setProcessoDetecao(ProcessoDetecao processoDetecao);
 
     /**
-     * Inicia o Processo de Deteção de um detetável, recebendo uma lista de 
+     * Inicia o Processo de Deteção de um detetável, recebendo uma lista de
      * tipos de conflito.
      *
-     * @param listaTiposConflito Lista de tipos de conflito. 
+     * @param listaTiposConflito Lista de tipos de conflito.
      */
     public void iniciarProcessoDeteca(List<TipoConflito> listaTiposConflito);
+
+    /**
+     * Altera o estado do detetavel quando o mesmo atinge a data de fim de
+     * submissão.
+     *
+     * @return Verdadeiro se for possível alterar o estado e falso caso não
+     * seja.
+     */
+    public boolean setEmDetecao();
 
 }
