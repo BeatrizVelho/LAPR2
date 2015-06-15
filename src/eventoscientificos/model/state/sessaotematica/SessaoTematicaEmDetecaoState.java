efede8c72305376baa1ac2386dc6ea8f95199a21
Soraia Freitas <1140805@isep.ipp.pt>
2015-06-15 22:48:03 +0100

    Pequenos ajustes na classe

--- src/eventoscientificos/model/state/sessaotematica/SessaoTematicaEmDetecaoState.java
+++ src/eventoscientificos/model/state/sessaotematica/SessaoTematicaEmDetecaoState.java
@@ -158,7 +158,7 @@ public class SessaoTematicaEmDetecaoState implements SessaoTematicaState {
      */
     @Override
     public boolean validarEstado() {
-        return true;
+        return false;
     }
 
     /**
