c7f14134a8cb43fafff93cb58802c2b5fcaf8b0e
Soraia Freitas <1140805@isep.ipp.pt>
2015-06-15 16:07:24 +0100

    implementação de alguns métodos na classe conflito (getters)

--- src/eventoscientificos/model/Conflito.java
+++ src/eventoscientificos/model/Conflito.java
@@ -1,7 +1,9 @@
 package eventoscientificos.model;
 
+import java.util.List;
+
 /**
- * Representa uma instância de conflito através 
+ * Representa uma instância de conflito através
  *
  * @author G01
  */
@@ -20,21 +22,43 @@ public class Conflito {
     /**
      * Tipo do conflito.
      */
-    private TipoConflito tipoConflito;
+    private List<TipoConflito> ListaTipoConflito;
 
     /**
      * Constrói uma instância de Conflito recebendo um revisor, uma submissão e
      * um tipo de conflito.
-     * 
+     *
      * @param revisor Revisor do conflito.
      * @param submissao Submissão do conflito.
-     * @param tipoConflito Tipo do conflito.
+     * @param listaConflitos lista de Tipos do conflito.
      */
     public Conflito(
-            Revisor revisor, Submissao submissao, TipoConflito tipoConflito) {
+                        Revisor revisor, Submissao submissao, List<TipoConflito> listaConflitos) {
         this.revisor = revisor;
         this.submissao = submissao;
-        this.tipoConflito = tipoConflito;
-    }   
+        this.ListaTipoConflito = listaConflitos;
+    }
+/**
+ * Devolve o revisor associado ao conflito
+ * @return revisor
+ */
+    public Revisor getRevisor() {
+        return revisor;
+    }
+/**
+ * Devolve a submissão associada ao conflito
+ * @return submissao
+ */
+    public Submissao getSubmissao() {
+        return submissao;
+    }
 
+    /**
+     * Devolve os tipos de conflitos associados entre o revisor e artigo
+     *
+     * @return lista de conflitos
+     */
+    public List<TipoConflito> getListaTipoConflito() {
+        return ListaTipoConflito;
+    }
 }
