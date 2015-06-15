f545372f1d18b8686615fc15918efd95fa5d412a
Soraia Freitas <1140805@isep.ipp.pt>
2015-06-15 17:50:20 +0100

    Classe ProcessoDeteção completamente testada e implementados os métodos necessários para a implementação do uc11

--- src/eventoscientificos/model/ProcessoDetecao.java
+++ src/eventoscientificos/model/ProcessoDetecao.java
@@ -1,5 +1,6 @@
 package eventoscientificos.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,6 +17,7 @@ public class ProcessoDetecao {
      * Constrói uma instância de Processo de Deteção
      */
     public ProcessoDetecao() {
+        this.listaConflito = new ArrayList<>();
     }
 
     /**
@@ -27,4 +29,27 @@ public class ProcessoDetecao {
         return listaConflito;
     }
 
+    /**
+     * Compara dois objetos entre si. Comparando primariamente a posição de
+     * memória, seguida do conteudo e das classes as quais cada um deles
+     * pertence, e finalmente os seus atributos, lista de conflitos.
+     *
+     * @param outroObjecto Processo de deteção a comparar
+     * @return Verdadeiro caso os objetos comparados sejam iguais e falso caso
+     * não o sejam.
+     */
+    @Override
+    public boolean equals(Object outroObjecto) {
+        if (this == outroObjecto) {
+            return true;
+        }
+
+        if (outroObjecto == null || this.getClass() != outroObjecto.getClass()) {
+            return false;
+        }
+
+        ProcessoDetecao outroProcesso = (ProcessoDetecao) outroObjecto;
+
+        return listaConflito.equals(outroProcesso.listaConflito);
+    }
 }
