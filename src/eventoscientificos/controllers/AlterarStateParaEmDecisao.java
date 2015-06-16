3ab4dd8c5efb401545cfeb4757bbae1ed56a12d7
Pedro Moreira <1140781@isep.ipp.pt>
2015-06-16 11:21:42 +0100

    Criadas as classes responsáveis pelas 3 alterações de estado dos eventos e sessões temáticas, quando é atingida uma determinada data, ainda é necessária a implementação de cada uma.

---
+++ src/eventoscientificos/controllers/AlterarStateParaEmDecisao.java
@@ -0,0 +1,20 @@
+package eventoscientificos.controllers;
+
+import eventoscientificos.model.Decidivel;
+import java.util.TimerTask;
+
+/**
+ * @author G01
+ */
+public class AlterarStateParaEmDecisao extends TimerTask {
+
+    public AlterarStateParaEmDecisao(Decidivel decidivel) {
+
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
