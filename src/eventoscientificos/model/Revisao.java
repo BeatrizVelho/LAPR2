ac84404fc4fb8e5de68a6fa6d784d43057c3a9de
Soraia Freitas <1140805@isep.ipp.pt>
2015-06-18 16:13:54 +0100

    Classe teste Revisão - complemente testada e funcional

--- src/eventoscientificos/model/Revisao.java
+++ src/eventoscientificos/model/Revisao.java
@@ -18,6 +18,59 @@ public class Revisao {
     private Revisor revisor;
 
     /**
+     * Nível de perícia em relação ao tópico.
+     */
+    private int confiancaRevisor;
+
+    /**
+     * Adequação do artigo em relação ao evento.
+     */
+    private int adequacaoArtigo;
+
+    /**
+     * Orgininalidade do artigo.
+     */
+    private int originalidadeArtigo;
+
+    /**
+     * Qualidade do artigo submetido.
+     */
+    private int qualidadeArtigo;
+    /**
+     * Recomendação global de um artigo.
+     */
+    private int recomendacaoGlobal;
+    /**
+     * Texto justificativo da avaliação efetuada.
+     */
+    private String textoJustificativo;
+    /**
+     * Confiança do revisor em relação ao topico do artigo por omissão.
+     */
+    private static final int CONFIANCA_REVISOR_POR_OMISSAO = -1;
+    /**
+     * Adequação do artigo or omissão.
+     */
+    private static final int ADEQUACAO_ARTIGO_POR_OMISSAO = -1;
+    /**
+     * Orgininalidade do artigo por omissão.
+     */
+    private static final int ORIGINALIDADE_ARTIGO_POR_OMISSAO = -1;
+    /**
+     * Qualidade do artigo por omissão.
+     */
+    private static final int QUALIDADE_ARTIGO_POR_OMISSAO = -1;
+    /**
+     * Recomendação global por omissão.
+     */
+    private static final int RECOMENDACAO_GLOBAL_POR_OMISSAO = -1;
+
+    /**
+     * Breve texto justificativo por omissão.
+     */
+    private static final String BREVE_TEXTO_JUSTIFICATIVO_POR_OMISSAO = "";
+
+    /**
      * Constrói uma instância de revisão através dos valores recebidos por
      * parâmetro
      *
@@ -28,17 +81,173 @@ public class Revisao {
         this.submissao = submissao;
         this.revisor = revisor;
     }
-///**
-// * Modifica o Revisor da revisão
-// * @param revisor 
-// */
-//    public void setRevisor(Revisor revisor) {
-//        this.revisor = revisor;
-//    }
 //
-//    public void setSubmissao(Submissao submissao) {
+//    /**
+//     * Cria uma instância de revisão com os valores recebidos por parâmetro.
+//     *
+//     * @param submissao submissao associada
+//     * @param revisor revisor destacado
+//     * @param confiancaRevisor confiança do revisor
+//     * @param adequacaoArtigo adequação do artigo ao evento/sessão temática
+//     * @param originalidadeArtigo originalidade do artigo
+//     * @param qualidadeArtigo qualidade do artigo
+//     * @param recomendacaoGlobal recomendação global
+//     */
+//    public Revisao(Submissao submissao, Revisor revisor, int confiancaRevisor,
+//                        int adequacaoArtigo, int originalidadeArtigo,
+//                        int qualidadeArtigo, int recomendacaoGlobal,
+//                        String textoJustificativo) {
 //        this.submissao = submissao;
+//        this.revisor = revisor;
+//        this.confiancaRevisor = CONFIANCA_REVISOR_POR_OMISSAO;
+//        this.adequacaoArtigo = ADEQUACAO_ARTIGO_POR_OMISSAO;
+//        this.originalidadeArtigo = ORIGINALIDADE_ARTIGO_POR_OMISSAO;
+//        this.qualidadeArtigo = QUALIDADE_ARTIGO_POR_OMISSAO;
+//        this.recomendacaoGlobal = RECOMENDACAO_GLOBAL_POR_OMISSAO;
+//        this.textoJustificativo = BREVE_TEXTO_JUSTIFICATIVO_POR_OMISSAO;
 //    }
+
+    /**
+     * Modifica o valor de Adequação do Artigo.
+     *
+     * @param adequacaoArtigo novo valor da adequação do artigo.
+     */
+    public void setAdequacaoArtigo(int adequacaoArtigo) {
+        if (adequacaoArtigo < 6 && adequacaoArtigo >= 0) {
+            this.adequacaoArtigo = adequacaoArtigo;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Modifica o valor de confiança do revisor.
+     *
+     * @param confiancaRevisor novo valor da confiança do revisor.
+     */
+    public void setConfiancaRevisor(int confiancaRevisor) {
+        if (confiancaRevisor < 6 && confiancaRevisor >= 0) {
+            this.confiancaRevisor = confiancaRevisor;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Modifica o valor da originalidade dada ao artigo.
+     *
+     * @param originalidadeArtigo novo valor da originalidade.
+     */
+    public void setOriginalidadeArtigo(int originalidadeArtigo) {
+        if (originalidadeArtigo < 6 && originalidadeArtigo >= 0) {
+            this.originalidadeArtigo = originalidadeArtigo;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Modifica o valor da qualidade do artigo.
+     *
+     * @param qualidadeArtigo novo valor da qualidade do artigo.
+     */
+    public void setQualidadeArtigo(int qualidadeArtigo) {
+        if (qualidadeArtigo < 6 && qualidadeArtigo >= 0) {
+            this.qualidadeArtigo = qualidadeArtigo;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Modifica o valor da recomendação global do artigo.
+     *
+     * @param recomendacaoGlobal novo valor da recomendação do artigo.
+     */
+    public void setRecomendacaoGlobal(int recomendacaoGlobal) {
+        if (recomendacaoGlobal <= 2 && recomendacaoGlobal >= -2) {
+            this.recomendacaoGlobal = recomendacaoGlobal;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Modifica o texto justificativo da revisão do artigo.
+     *
+     * @param textoJustificativo novo texto justificativo.
+     */
+    public void setTextoJustificativo(String textoJustificativo) {
+        if (textoJustificativo.length() > 0) {
+            this.textoJustificativo = textoJustificativo;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Cria uma instância de clone à instãncia que o invoca
+     *
+     * @return Revisao
+     */
+    public Revisao criarCloneRevisao() {
+        return new Revisao(this.submissao, this.revisor);
+    }
+
+    /**
+     * Adiciona os dados da revisão introduzidos pelo utilizador.
+     *
+     * @param adequacao
+     * @param confianca
+     * @param originalidade
+     * @param qualidade
+     * @param recomendacao
+     * @param textoJustificativo
+     * @return
+     */
+    public boolean adicionarDadosRevisao(int adequacao, int confianca,
+                        int originalidade, int qualidade, int recomendacao,
+                        String textoJustificativo) {
+        setAdequacaoArtigo(adequacao);
+        setConfiancaRevisor(confianca);
+        setOriginalidadeArtigo(originalidade);
+        setQualidadeArtigo(qualidade);
+        setRecomendacaoGlobal(recomendacao);
+        setTextoJustificativo(textoJustificativo);
+        return true;
+    }
+
+    /**
+     * Adiciona à revisão os valores de revisão atribuidos pelo revisor ao
+     * artigo
+     *
+     * @param revisao revisão à qual vai se adicionar os novos valores
+     * @return verdadeiro se modificar todos os valores correctamente e falso se
+     * não o conseguir.
+     */
+    public boolean adicionarResultadoRevisao(Revisao revisao) {
+        revisao.setAdequacaoArtigo(this.adequacaoArtigo);
+        revisao.setConfiancaRevisor(this.confiancaRevisor);
+        revisao.setOriginalidadeArtigo(this.originalidadeArtigo);
+        revisao.setQualidadeArtigo(this.qualidadeArtigo);
+        revisao.setRecomendacaoGlobal(this.recomendacaoGlobal);
+        revisao.setTextoJustificativo(textoJustificativo);
+        return true;
+    }
+
+    /**
+     * Valida se a revisão se encontra devidamente preenchida
+     *
+     * @return verdadeiro se for uma revisão válida e se
+     */
+    public boolean validarRevisao() {
+        return (this.adequacaoArtigo != ADEQUACAO_ARTIGO_POR_OMISSAO
+                            && this.confiancaRevisor != CONFIANCA_REVISOR_POR_OMISSAO
+                            && this.originalidadeArtigo != ORIGINALIDADE_ARTIGO_POR_OMISSAO
+                            && this.qualidadeArtigo != QUALIDADE_ARTIGO_POR_OMISSAO
+                            && this.recomendacaoGlobal != RECOMENDACAO_GLOBAL_POR_OMISSAO
+                            && !this.textoJustificativo.equalsIgnoreCase(textoJustificativo));
+    }
 
     /**
      * Compara dois objetos entre si. Comparando primariamente a posição de
