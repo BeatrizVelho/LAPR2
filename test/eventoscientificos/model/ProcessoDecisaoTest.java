af913284d2a28a07e25342809e84e9e8bdfdd51c
TimiFerreira <1131658@isep.ipp.pt>
2015-06-21 22:42:55 +0100

    Implementação dos testes dos métodos getListaRevisoes() e adicionarMecanismoDecisao(), da classe ProcessoDecisaoTest, ficando a faltar o teste do método equals().

---
+++ test/eventoscientificos/model/ProcessoDecisaoTest.java
@@ -0,0 +1,65 @@
+package eventoscientificos.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Teste à classe ProcessoDecisao.
+ * 
+ * @author G01
+ */
+public class ProcessoDecisaoTest {
+    
+    private Submissao submissao;
+    private Revisor revisor;
+    
+    public ProcessoDecisaoTest() {
+        Artigo artigoInicial = new Artigo();
+        Artigo artigoFinal = new Artigo();
+        this.submissao = new Submissao();
+        
+        this.revisor = new Revisor(new Utilizador("nome",
+                            "1140587@isep.ipp.pt",
+                            "username",
+                            "password"));
+    }
+
+    /**
+     * Teste do método getListaRevisoes, da classe ProcessoDecisao.
+     */
+    @Test
+    public void testGetListaRevisoes() {
+        System.out.println("getListaRevisoes");
+        ProcessoDecisao instance = new ProcessoDecisao();
+        ListaRevisoes expResult = null;
+        ListaRevisoes result = instance.getListaRevisoes();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Teste do método adicionarMecanismoDecisao, da classe ProcessoDecisao.
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testAdicionarMecanismoDecisao() {
+        System.out.println("adicionarMecanismoDecisao");
+        MecanismoDecisao mecanismoDecisao = null;
+        ProcessoDecisao instance = new ProcessoDecisao();
+        boolean expResult = false;
+        boolean result = instance.adicionarMecanismoDecisao(mecanismoDecisao);
+        assertEquals(expResult, result);
+    }
+
+//    /**
+//     * Teste do método equals, da classe ProcessoDecisao.
+//     */
+//    @Test
+//    public void testEquals() {
+//        System.out.println("equals");
+//        Object outroObjecto = null;
+//        ProcessoDecisao instance = new ProcessoDecisao();
+//        boolean expResult = false;
+//        boolean result = instance.equals(outroObjecto);
+//        assertEquals(expResult, result);
+//    }
+   
+}
