f5789d2202a3fac96b8de0879bf7f36be502f8f6
Pedro Moreira <1140781@isep.ipp.pt>
2015-06-24 00:23:12 +0100

    Removidas duas linhas de código desnecessárias.

--- src/eventoscientificos/model/ListaAutores.java
+++ src/eventoscientificos/model/ListaAutores.java
@@ -5,23 +5,23 @@ import java.util.List;
 
 /**
  * Representa uma instância de lista de autores através de uma lista de autores
- * 
+ *
  * @author G01
  */
 public class ListaAutores {
-    
+
     /**
      * Lista de autores.
      */
     private List<Autor> listaAutores;
-    
+
     /**
      * Constrói uma instância de uma lista de autores vazia.
      */
     public ListaAutores() {
         this.listaAutores = new ArrayList();
     }
-    
+
     /**
      * Cria um objeto do tipo Autor através de um nome, email e uma instituição
      * de afiliação.
@@ -29,123 +29,120 @@ public class ListaAutores {
      * @param nome Nome do autor.
      * @param email Email do autor.
      * @param instituicaoAfiliacao instituicao de afiliacao do autor
-     * 
+     *
      * @return Verdadeiro que o Autor criado for adicionado à lista e falso se
      * não for.
      */
-    public boolean novoAutor(String nome, String email, 
+    public boolean novoAutor(String nome, String email,
             InstituicaoAfiliacao instituicaoAfiliacao) {
-        
+
         Autor autor = new Autor(nome, email, instituicaoAfiliacao);
 
-        if(!autor.validarAutor()) {
+        if (!autor.validarAutor()) {
             throw new IllegalArgumentException(
                     "Não introduziu um autor válido.");
         }
-        if(!validarAutor(autor)) {
+        if (!validarAutor(autor)) {
             throw new IllegalArgumentException("O autor introduzido já se"
                     + "econtra na lista de autores.");
         }
-        
+
         return adicionarAutor(autor);
     }
-    
+
     /**
      * Cria um objeto do tipo Autor através de um utilizador e uma instituição
      * de afiliação.
-     * 
+     *
      * @param utilizador Utilizador que é autor.
      * @param instituicaoAfiliacao Instituicao de Afiliacao do autor.
-     * 
+     *
      * @return Verdadeiro que o Autor criado for adicionado à lista e falso se
      * não for.
      */
-    public  boolean novoAutor(
+    public boolean novoAutor(
             Utilizador utilizador, InstituicaoAfiliacao instituicaoAfiliacao) {
         Autor autor = new Autor(utilizador, instituicaoAfiliacao);
-        System.out.println("Imprimir lista");
-        System.out.println(listaAutores);
-        if(!autor.validarAutor()) {
+
+        if (!autor.validarAutor()) {
             throw new IllegalArgumentException(
                     "Não introduziu um autor válido.");
         }
-        if(!validarAutor(autor)) {
+        if (!validarAutor(autor)) {
             throw new IllegalArgumentException("O autor introduzido já se "
                     + "encontra na lista de autores.");
         }
-        
+
         return adicionarAutor(autor);
     }
-    
+
     /**
-     * Valida uma instância de Autor verificando se o mesmo já existe
-     * na lista.
-     * 
+     * Valida uma instância de Autor verificando se o mesmo já existe na lista.
+     *
      * @param autor Autor que vai ser procurado na lista.
-     * @return Verdadeiro se o Autor não existir na lista e falso caso
-     *exista.
+     * @return Verdadeiro se o Autor não existir na lista e falso caso exista.
      */
     private boolean validarAutor(Autor autor) {
         return !this.listaAutores.contains(autor);
     }
-    
+
     /**
      * Adicina um autor a lista de autores.
-     * 
+     *
      * @param autor Autor a inserir na lista de autores.
-     * 
+     *
      * @return Verdadeiro se for adicionado e falso se não for.
      */
     private boolean adicionarAutor(Autor autor) {
         return this.listaAutores.add(autor);
     }
-    
+
     /**
      * Remove um autor da lista de autores.
-     * 
+     *
      * @param autor Autor a remover da lista de autores.
      * @return Verdadeiro se for removido e falso se não for
      */
     public boolean removerAutor(String email) {
-        for(Autor autor : this.listaAutores) {
-            if(autor.getEmail().equals(email)) {
+        for (Autor autor : this.listaAutores) {
+            if (autor.getEmail().equals(email)) {
                 return this.listaAutores.remove(autor);
             }
         }
         return false;
     }
-    
+
     /**
-     * Verifica se o utilizador passado por parâmetro está inserido na lista
-     * de autores.
-     * 
+     * Verifica se o utilizador passado por parâmetro está inserido na lista de
+     * autores.
+     *
      * @param utilizador Utilizador a verificar.
      * @return Verdadeiro se já existe na lista de autores e falso se não está.
      */
-    public boolean isAutor(Utilizador utilizador){
-        for(Autor autor : this.listaAutores) {
-            if(utilizador.equals(autor.getUtilizador())){
+    public boolean isAutor(Utilizador utilizador) {
+        for (Autor autor : this.listaAutores) {
+            if (utilizador.equals(autor.getUtilizador())) {
                 return true;
             }
         }
         return false;
     }
-    
+
     /**
      * Devolve uma lista de autores registados na lista de autores.
-     * 
+     *
      * @return Lista de Autores registados
      */
     public List<Autor> getListaAutoresRegistados() {
-       List<Autor> listaAutoresRegistados = new ArrayList<>();
-        for(Autor autor : this.listaAutores) {
-            if(autor.isAutorRegistado()) {
+        List<Autor> listaAutoresRegistados = new ArrayList<>();
+        for (Autor autor : this.listaAutores) {
+            if (autor.isAutorRegistado()) {
                 listaAutoresRegistados.add(autor);
             }
         }
         return listaAutoresRegistados;
     }
-    
+
     /**
      * Compara dois objetos entre si. Comparando primariamente a posição de
      * memória, seguida do conteudo e das classes as quais cada um deles
