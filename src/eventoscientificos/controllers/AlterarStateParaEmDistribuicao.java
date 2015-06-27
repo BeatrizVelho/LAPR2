36f0a5c63cb0a51e97b4acd36d020163341ec544
Beatriz Velho <1140587@isep.ipp.pt>
2015-06-27 06:22:21 +0100

    Classe AlterarStateParaEmDistribuicao

--- src/eventoscientificos/controllers/AlterarStateParaEmDistribuicao.java
+++ src/eventoscientificos/controllers/AlterarStateParaEmDistribuicao.java
@@ -4,17 +4,34 @@ import eventoscientificos.model.Distribuivel;
 import java.util.TimerTask;
 
 /**
+ * Representa uma instância de AlterarStateParaEmDistribuicao através de um
+ * distribuivel
+ * 
  * @author G01
  */
 public class AlterarStateParaEmDistribuicao extends TimerTask {
 
+    /**
+     * Distribuivel que é alterado para em distribuição.
+     */
+    private Distribuivel distribuivel;
+    
+    /**
+     * Constrói uma instância de AlterarStateParaEmDistribuicao recebendo um
+     * distribuivel.
+     * 
+     * @param distribuivel Distribuivel.
+     */
     public AlterarStateParaEmDistribuicao(Distribuivel distribuivel) {
-        
+        this.distribuivel = distribuivel;
     }
 
+     /**
+     * Método que despoleta o processo de alteração de estado do distribuivel.
+     */
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.distribuivel.setEmDistribuicao();
     }
     
 }
