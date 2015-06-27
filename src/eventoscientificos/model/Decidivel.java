e091db20fe68824f9ee21c5b24ec6316c7fec652
Beatriz Velho <1140587@isep.ipp.pt>
2015-06-27 06:22:55 +0100

    setEmDecisao() - Decidivel

--- src/eventoscientificos/model/Decidivel.java
+++ src/eventoscientificos/model/Decidivel.java
@@ -38,4 +38,12 @@ public interface Decidivel {
      */
     public boolean adicionarProcessoDecisao(ProcessoDecisao processoDecisao);
     
+    /**
+     * Altera o estado do decidivel quando o mesmo atinge a data de fim de 
+     * revisão.
+     * 
+     * @return Verdadeiro se for possível alterar o estado e falso caso não 
+     * seja.
+     */
+    boolean setEmDecisao();
 }
