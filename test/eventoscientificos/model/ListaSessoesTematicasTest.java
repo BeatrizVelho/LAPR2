89a4c93a4bb5b21c0ac2369386ab197cb45a60a7
Beatriz Velho <1140587@isep.ipp.pt>
2015-06-27 22:45:11 +0100

    ListaSessoesTematicasTest

--- test/eventoscientificos/model/ListaSessoesTematicasTest.java
+++ test/eventoscientificos/model/ListaSessoesTematicasTest.java
@@ -1,17 +1,24 @@
 package eventoscientificos.model;
 
+import eventoscientificos.model.state.evento.EventoEmSubmissaoCameraReadyState;
+import eventoscientificos.model.state.sessaotematica.SessaoTematicaEmSubmissaoCameraReadyState;
 import eventoscientificos.model.state.sessaotematica.SessaoTematicaEmSubmissaoState;
 import eventoscientificos.model.state.sessaotematica.SessaoTematicaFaseDecisaoState;
+import eventoscientificos.model.state.submissao.SubmissaoAceiteState;
+import eventoscientificos.model.state.submissao.SubmissaoEmSubmissaoState;
+import eventoscientificos.model.state.submissao.SubmissaoRejeitadaState;
 import eventoscientificos.model.state.submissao.SubmissaoRemovidaState;
 import eventoscientificos.model.state.submissao.SubmissaoRevistaState;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import utils.Data;
 
 /**
- *Teste à classe ListaSessoesTematicas.
+ * Teste à classe ListaSessoesTematicas.
+ *
  * @author G01
  */
 public class ListaSessoesTematicasTest {
@@ -20,7 +27,7 @@ public class ListaSessoesTematicasTest {
     private ListaSessoesTematicas listaSessoesTematicas;
     private SessaoTematica sessaoTematica;
     private Submissao submissao;
-    
+
     public ListaSessoesTematicasTest() {
         this.evento = new Evento("titulo", "descricao", new Local("local"),
                 new Data(2016, 6, 8), new Data(2016, 6, 20),
@@ -47,10 +54,10 @@ public class ListaSessoesTematicasTest {
         System.out.println("novaSessaoTematica");
         SessaoTematica expResult = this.sessaoTematica;
         SessaoTematica result = instance.novaSessaoTematica(
-                            "#123456", "Uma descrição", new Data(2015, 5, 22),
-                            new Data(2015, 5, 28), new Data(2015, 6, 20),
-                            new Data(2015, 6, 24), new Data(2015, 6, 28),
-                            new Data(2015, 7, 1), new Data(2015, 7, 7));
+                "#123456", "Uma descrição", new Data(2015, 5, 22),
+                new Data(2015, 5, 28), new Data(2015, 6, 20),
+                new Data(2015, 6, 24), new Data(2015, 6, 28),
+                new Data(2015, 7, 1), new Data(2015, 7, 7));
         assertEquals(expResult, result);
     }
 
@@ -148,8 +155,8 @@ public class ListaSessoesTematicasTest {
     }
 
     /**
-     * Teste do método getListaCPDefiniveisSemCPOrganizadorProponente,
-     * da classe ListaSessoesTematicas.
+     * Teste do método getListaCPDefiniveisSemCPOrganizadorProponente, da classe
+     * ListaSessoesTematicas.
      */
     @Test
     public void testGetListaCPDefiniveisSemCPOrganizadorProponente() {
@@ -164,7 +171,8 @@ public class ListaSessoesTematicasTest {
     }
 
     /**
-     * Teste do método getListaSessoesTematicas, da classe ListaSessoesTematicas.
+     * Teste do método getListaSessoesTematicas, da classe
+     * ListaSessoesTematicas.
      */
     @Test
     public void testGetListaSessoesTematicas() {
@@ -172,17 +180,18 @@ public class ListaSessoesTematicasTest {
         ListaSessoesTematicas listaSessoesTematicas = this.listaSessoesTematicas;
         listaSessoesTematicas.adicionarSessaoTematica(this.sessaoTematica);
         listaSessoesTematicas.adicionarSessaoTematica(new SessaoTematica(
-                            "#1234567", "Uma descrição", new Data(2016, 6, 9),
-                            new Data(2016, 6, 21), new Data(2016, 7, 8),
-                            new Data(2016, 7, 9), new Data(2017, 3, 25),
-                            new Data(2017, 5, 29), new Data(2017, 6, 1)));
+                "#1234567", "Uma descrição", new Data(2016, 6, 9),
+                new Data(2016, 6, 21), new Data(2016, 7, 8),
+                new Data(2016, 7, 9), new Data(2017, 3, 25),
+                new Data(2017, 5, 29), new Data(2017, 6, 1)));
         int expResult = 2;
         int result = listaSessoesTematicas.getListaSessoesTematicas().size();
         assertEquals(expResult, result);
     }
 
     /**
-     * Teste do método getListaSubmissiveisAceitarArtigo, da classe ListaSessoesTematicas.
+     * Teste do método getListaSubmissiveisAceitarArtigo, da classe
+     * ListaSessoesTematicas.
      */
     @Test
     public void testGetListaSubmissiveisAceitarArtigo() {
@@ -190,10 +199,10 @@ public class ListaSessoesTematicasTest {
         ListaSessoesTematicas instance = this.listaSessoesTematicas;
         SessaoTematica sessaoTematica = this.sessaoTematica;
         SessaoTematica sessaoTematica1 = new SessaoTematica(
-                            "#1234567", "Uma descrição", new Data(2016, 6, 9),
-                            new Data(2016, 6, 21), new Data(2016, 7, 8),
-                            new Data(2016, 7, 9), new Data(2017, 3, 25),
-                            new Data(2017, 5, 29), new Data(2017, 6, 1));
+                "#1234567", "Uma descrição", new Data(2016, 6, 9),
+                new Data(2016, 6, 21), new Data(2016, 7, 8),
+                new Data(2016, 7, 9), new Data(2017, 3, 25),
+                new Data(2017, 5, 29), new Data(2017, 6, 1));
         sessaoTematica.setEstado(new SessaoTematicaEmSubmissaoState(sessaoTematica));
         instance.adicionarSessaoTematica(sessaoTematica);
         instance.adicionarSessaoTematica(sessaoTematica1);
@@ -203,7 +212,8 @@ public class ListaSessoesTematicasTest {
     }
 
     /**
-     * Teste do método temSessoesTematicasDefinidas, da classe ListaSessoesTematicas.
+     * Teste do método temSessoesTematicasDefinidas, da classe
+     * ListaSessoesTematicas.
      */
     @Test
     public void testTemSessoesTematicasDefinidas() {
@@ -211,10 +221,10 @@ public class ListaSessoesTematicasTest {
         ListaSessoesTematicas instance = this.listaSessoesTematicas;
         SessaoTematica sessaoTematica = this.sessaoTematica;
         SessaoTematica sessaoTematica1 = new SessaoTematica(
-                            "#1234567", "Uma descrição", new Data(2016, 6, 9),
-                            new Data(2016, 6, 21), new Data(2016, 7, 8),
-                            new Data(2016, 7, 9), new Data(2017, 3, 25),
-                            new Data(2017, 5, 29), new Data(2017, 6, 1));
+                "#1234567", "Uma descrição", new Data(2016, 6, 9),
+                new Data(2016, 6, 21), new Data(2016, 7, 8),
+                new Data(2016, 7, 9), new Data(2017, 3, 25),
+                new Data(2017, 5, 29), new Data(2017, 6, 1));
         instance.adicionarSessaoTematica(sessaoTematica);
         instance.adicionarSessaoTematica(sessaoTematica1);
         boolean expResult = true;
@@ -223,8 +233,9 @@ public class ListaSessoesTematicasTest {
     }
 
     /**
-     * Teste do método getListaSubmissiveisAceitarAlteracaoArtigoComSubmissaoUtilizador,
- da classe ListaSessoesTematicas.
+     * Teste do método
+     * getListaSubmissiveisAceitarAlteracaoArtigoComSubmissaoUtilizador, da
+     * classe ListaSessoesTematicas.
      */
     @Test
     public void testGetListaSubmissiveisAceitarArtigoComSubmissaoUtilizador() {
@@ -234,14 +245,14 @@ public class ListaSessoesTematicasTest {
         Utilizador utilizador = new Utilizador(
                 "pedro", "1140781@isep.ipp.pt", "pedro", "12345");
         SessaoTematica sessaoTematica1 = new SessaoTematica(
-                            "#1234567", "Uma descrição", new Data(2016, 6, 9),
-                            new Data(2016, 6, 21), new Data(2016, 7, 8),
-                            new Data(2016, 7, 9), new Data(2017, 3, 25),
-                            new Data(2017, 5, 29), new Data(2017, 6, 1));
+                "#1234567", "Uma descrição", new Data(2016, 6, 9),
+                new Data(2016, 6, 21), new Data(2016, 7, 8),
+                new Data(2016, 7, 9), new Data(2017, 3, 25),
+                new Data(2017, 5, 29), new Data(2017, 6, 1));
         ListaSessoesTematicas instance = this.listaSessoesTematicas;
         Submissao submissao = new Submissao();
         submissao.setArtigoInicial(new Artigo());
-        submissao .getArtigoInicial().getListaAutores().novoAutor(utilizador, new InstituicaoAfiliacao("ISEP"));
+        submissao.getArtigoInicial().getListaAutores().novoAutor(utilizador, new InstituicaoAfiliacao("ISEP"));
         sessaoTematica.getListaSubmissoes().adicionarSubmissao(submissao);
         instance.adicionarSessaoTematica(sessaoTematica);
         instance.adicionarSessaoTematica(sessaoTematica1);
@@ -249,7 +260,7 @@ public class ListaSessoesTematicasTest {
         int result = instance.getListaSubmissiveisAceitarArtigoComSubmissaoUtilizador(utilizador).size();
         assertEquals(expResult, result);
     }
-    
+
     /**
      * Teste do método
      * getListaSubmissiveisComSubmissoesRetiradasOrganizadorProponente, da class
@@ -272,7 +283,8 @@ public class ListaSessoesTematicasTest {
     }
 
     /**
-     * Teste do método getNumeroSessoesTematicas, da classe ListaSessoesTematicas.
+     * Teste do método getNumeroSessoesTematicas, da classe
+     * ListaSessoesTematicas.
      */
     @Test
     public void testGetNumeroSessoesTematicas() {
@@ -285,7 +297,8 @@ public class ListaSessoesTematicasTest {
     }
 
     /**
-     * Teste do método getSessoesTematicasPeloID, da classe ListaSessoesTematicas.
+     * Teste do método getSessoesTematicasPeloID, da classe
+     * ListaSessoesTematicas.
      */
     @Test
     public void testGetSessoesTematicasPeloID() {
@@ -299,8 +312,8 @@ public class ListaSessoesTematicasTest {
     }
 
     /**
-     * Teste do método getListaDecidivelOrganizadorProponente,
-     * da classe ListaSessoesTematicas.
+     * Teste do método getListaDecidivelOrganizadorProponente, da classe
+     * ListaSessoesTematicas.
      */
     @Test
     public void testGetListaDecidivelOrganizadorProponente() {
@@ -320,7 +333,8 @@ public class ListaSessoesTematicasTest {
     }
 
     /**
-     * Test of getSessaoTematicaComCodigoUnico method, of class ListaSessoesTematicas.
+     * Test of getSessaoTematicaComCodigoUnico method, of class
+     * ListaSessoesTematicas.
      */
     @Test
     public void testGetSessaoTematicaComCodigoUnico() {
@@ -333,9 +347,10 @@ public class ListaSessoesTematicasTest {
         SessaoTematica result = instance.getSessaoTematicaComCodigoUnico(codigoUnico);
         assertEquals(expResult, result);
     }
-    
+
     /**
-     * Test of getSessaoTematicaComCodigoUnico method, of class ListaSessoesTematicas.
+     * Test of getSessaoTematicaComCodigoUnico method, of class
+     * ListaSessoesTematicas.
      */
     @Test
     public void testGetSessaoTematicaComCodigoUnicoNull() {
@@ -347,5 +362,131 @@ public class ListaSessoesTematicasTest {
         SessaoTematica result = instance.getSessaoTematicaComCodigoUnico(codigoUnico);
         assertEquals(expResult, result);
     }
-    
+
+    /**
+     * Test of getListaSubmissiveisAceitarAlteracaoArtigoComSubmissaoUtilizador
+     * method, of class ListaSessoesTematicas.
+     */
+    @Test
+    public void testGetListaSubmissiveisAceitarAlteracaoArtigoComSubmissaoUtilizador() {
+        System.out.println("getListaSubmissiveisAceitarAlteracaoArtigoComSubmissaoUtilizador");
+        Utilizador utilizador = new Utilizador(
+                "bea", "1140587@isep.ipp.pt", "bea", "12345");;
+        Evento evento = this.evento;
+        SessaoTematica st = new SessaoTematica(
+                "#A9D24R",
+                "LAPR2",
+                new Data(2015, 5, 22),
+                new Data(2015, 5, 28),
+                new Data(2015, 6, 10),
+                new Data(2015, 6, 20),
+                new Data(2015, 6, 24),
+                new Data(2015, 6, 28),
+                new Data(2015, 6, 30));
+        st.setEstado(new SessaoTematicaEmSubmissaoState(st));
+        evento.setEstado(new EventoEmSubmissaoCameraReadyState(evento));
+        Submissao submissao = new Submissao();
+
+        Artigo artigo = new Artigo();
+        artigo.getListaAutores().novoAutor(utilizador, new InstituicaoAfiliacao("ISEP"));
+        submissao.setArtigoInicial(artigo);
+        st.getListaSubmissoes().adicionarSubmissao(submissao);
+        submissao.setEstado(new SubmissaoAceiteState(submissao));
+        submissao.setEstado(new SubmissaoEmSubmissaoState(submissao));
+        evento.getListaSessoesTematicas().adicionarSessaoTematica(st);
+        evento.getListaSubmissoes().adicionarSubmissao(submissao);
+
+        ListaSessoesTematicas instance = new ListaSessoesTematicas(evento);
+        int expResult = 1;
+        int result = evento.getListaSubmissiveisAceitarAlteracaoArtigoComSubmissaoUtilizador(utilizador).size();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getListaSubmissiveisAceitarArtigoFinal method, of class
+     * ListaSessoesTematicas.
+     */
+    @Test
+    public void testGetListaSubmissiveisAceitarArtigoFinal() {
+        System.out.println("getListaSubmissiveisAceitarArtigoFinal");
+        Utilizador utilizador = new Utilizador(
+                "Bea", "1140781@isep.ipp.pt", "bea", "12345");;
+        Evento instance = this.evento;
+        SessaoTematica st = new SessaoTematica(
+                "#A9D24R",
+                "LAPR2",
+                new Data(2015, 5, 22),
+                new Data(2015, 5, 28),
+                new Data(2015, 6, 10),
+                new Data(2015, 6, 20),
+                new Data(2015, 6, 24),
+                new Data(2015, 6, 28),
+                new Data(2015, 6, 30));
+        st.setEstado(new SessaoTematicaEmSubmissaoCameraReadyState(st));
+        instance.setEstado(new EventoEmSubmissaoCameraReadyState(evento));
+        Submissao submissao = new Submissao();
+        submissao.setEstado(new SubmissaoEmSubmissaoState(submissao));
+        Artigo artigo = new Artigo();
+        artigo.getListaAutores().novoAutor(utilizador, new InstituicaoAfiliacao("ISEP"));
+        submissao.setArtigoInicial(artigo);
+        st.getListaSubmissoes().adicionarSubmissao(submissao);
+        submissao.setEstado(new SubmissaoAceiteState(submissao));
+        evento.getListaSessoesTematicas().adicionarSessaoTematica(st);
+        instance.getListaSubmissoes().adicionarSubmissao(submissao);
+        int expResult = 1;
+        int result = instance.getListaSubmissiveisAceitarArtigoFinal(utilizador).size();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of hashMapSubmissoesSessaoTematica method, of class
+     * ListaSessoesTematicas.
+     */
+    @Test
+    public void testHashMapSubmissoesSessaoTematica() {
+        System.out.println("hashMapSubmissoesSessaoTematica");
+        Evento evento = new Evento("titulo", "descricao", new Local("local"),
+                new Data(2016, 6, 8), new Data(2016, 6, 20),
+                new Data(2016, 7, 7), new Data(2016, 8, 15),
+                new Data(2016, 9, 10), new Data(2016, 10, 1),
+                new Data(2017, 6, 10));
+
+        List<String> palavrasChaves = new ArrayList<>();
+        palavrasChaves.add("praia");
+        Submissao submissao = new Submissao();
+        Artigo artigo = new Artigo();
+        artigo.getListaAutores().novoAutor(new Utilizador("nome", "sad@isep.pt", "isernamo", "FVD"), new InstituicaoAfiliacao("ISEP"));
+        artigo.setPalavrasChave(palavrasChaves);
+        submissao.setArtigoInicial(artigo);
+
+        Revisao revisao = new Revisao(submissao, new Revisor(new Utilizador(
+                "bea", "1140781@isep.ipp.pt", "pedro", "12345")));
+        revisao.setRecomendacaoGlobal(1);
+        submissao.setEstado(new SubmissaoAceiteState(submissao));
+
+        List<String> palavrasChaves1 = new ArrayList<>();
+        palavrasChaves1.add("sumo");
+        Submissao submissao1 = new Submissao();
+        Artigo artigo1 = new Artigo();
+        artigo1.setTitulo("isep");
+        artigo1.getListaAutores().novoAutor(new Utilizador("nome", "sad@isep.pt", "isernamo", "FVD"), new InstituicaoAfiliacao("ISEP"));
+        artigo1.setPalavrasChave(palavrasChaves1);
+        submissao1.setArtigoInicial(artigo1);
+        Revisao revisao1 = new Revisao(submissao1, new Revisor(new Utilizador(
+                "bea", "1140781@isep.ipp.pt", "pedro", "12345")));
+        revisao1.setRecomendacaoGlobal(0);
+        submissao1.setEstado(new SubmissaoRejeitadaState(submissao1));
+
+        ProcessoDistribuicao processo = new ProcessoDistribuicao();
+        processo.getListaRevisoes().adicionarRevisao(revisao);
+        processo.getListaRevisoes().adicionarRevisao(revisao1);
+        evento.adicionarProcessoDistribuicao(processo);
+
+        evento.setEstado(new EventoEmSubmissaoCameraReadyState(evento));
+        HashMap<String, Integer> hashMapSubmissoesAceites = new HashMap<>();
+        HashMap<String, Integer> hashMapSubmissoesRejeitadas = new HashMap<>();
+        ListaSessoesTematicas instance = new ListaSessoesTematicas(evento);
+        instance.hashMapSubmissoesSessaoTematica(hashMapSubmissoesAceites, hashMapSubmissoesRejeitadas);
+    }
+
 }
